9bd37be16de708f7807436501fcc246d9e52e7b9
arthuras-k <a-r-ch@yandex.ru>
2022-07-04 12:02:42 +0300

    added task_2

--- netology_py_hw_8/task_1.py
+++ netology_py_hw_8/task_1.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 def smartest(hero1, hero2, hero3):
     url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
@@ -12,8 +11,11 @@ def smartest(hero1, hero2, hero3):
         elif hero2 == name_hero:
             dict_hero[hero2] = response.json()[num]['powerstats']['intelligence']
         elif hero3 == name_hero:
-            dict_hero[hero3] = response.json()[num]['powerstats']['intelligence']  
-    return max(dict_hero, key = lambda x: dict_hero[x])
+            dict_hero[hero3] = response.json()[num]['powerstats']['intelligence']
+    if len(dict_hero) == 3:
+        return max(dict_hero, key = lambda x: dict_hero[x])
+    else:
+        return "Ошибка! Одного или нескольких персонажей с данным именем нет в БД"
 
 if __name__ == '__main__':
     smartest_hero = smartest('Hulk', 'Captain America', 'Thanos')
